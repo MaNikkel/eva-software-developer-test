4b499f4c61447b1972819296e038abf609deb2ac
Mathias Nikkel <mathiasnikkel@gmail.com>
2023-02-28 20:22:01 -0300

    feat: using cors

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,8 +1,11 @@
 import express, { Response, Request } from 'express';
 import indexRouter from './routes';
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(express.static('public'));
 app.use('/', indexRouter);
