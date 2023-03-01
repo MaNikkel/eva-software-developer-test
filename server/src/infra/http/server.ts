5eb21febfbcc9147fcd413d290b87b0e5977d58c
Mathias Nikkel <mathiasnikkel@gmail.com>
2023-03-01 07:26:01 -0300

    fix: validation improvment

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,4 +1,4 @@
-import express, { Response, Request } from 'express';
+import express, { Response, Request, NextFunction } from 'express';
 import indexRouter from './routes';
 import cors from 'cors';
 const app = express();
@@ -10,19 +10,12 @@ app.use(cors());
 app.use(express.static('public'));
 app.use('/', indexRouter);
 
-process.on('unhandledRejection', (error: Error) => {
-  console.error(error);
-});
-
-process.on('uncaughtException', (error: Error) => {
-  console.error(error);
-});
-
-// Error handler
-app.use((error: Error, req: Request, res: Response) => {
-  console.error(error);
-
-  return res.status(500).json({ message: error.message, req: req.url });
+app.use((err, _: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render('error', { error: err });
 });
 
 export default app;
--- server/src/infra/http/validation/link-employee-journey.schema.ts
+++ server/src/infra/http/validation/link-employee-journey.schema.ts
@@ -2,7 +2,9 @@ import Joi from 'joi';
 import { AvailableJourneys } from '../../../domain/types/available-journeys-slugs';
 
 export const employeeJourneySchema = Joi.object().keys({
-  employeeId: Joi.string().uuid(),
+  employeeId: Joi.string().uuid().required(),
   startDate: Joi.date().required(),
-  journeySlug: Joi.string().valid(...Object.values(AvailableJourneys)),
+  journeySlug: Joi.string()
+    .required()
+    .valid(...Object.values(AvailableJourneys)),
 });
